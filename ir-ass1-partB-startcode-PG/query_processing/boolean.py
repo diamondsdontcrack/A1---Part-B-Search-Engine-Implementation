279803a49654faeff12eea7c6ec7096e678faf9a
Diamond <50936578+diamondsdontcrack@users.noreply.github.com>
2026-09-12 16:49:42 +1000

    Update boolean.py
    Completed shunting_yard_reorder and _evaluate_postfix functions
    
    Passed 3 additional Task 1 checkers and Task 3: no_optimisation preserves wildcard query syntax

--- ir-ass1-partB-startcode-PG/query_processing/boolean.py
+++ ir-ass1-partB-startcode-PG/query_processing/boolean.py
@@ -7,13 +7,48 @@ from index.access import get_all_doc_ids, get_posting_list
 
 def _shunting_yard_reorder(tokens: List[str]) -> List[str]:
     """Reorder infix Boolean tokens into postfix form."""
+
+    # queue uses FIFO, stack uses LIFO
     output_queue: List[str] = []
     operator_stack: List[str] = []
+    precedence = {"NOT": 3, "AND": 2, "OR": 1} # and brackets come first, when operator_stack encounters a ")", pop stack into output_queue until "("
+    # e.g. if AND already exists in operator_stack, can't push OR -> pop AND first into output_queue then push OR into stack
+    # given (climate OR machine) AND NOT policy -> [climate, machine, OR, policy, NOT, AND]
 
     # See the Week 04 lecture section "Shunting-Yard Algorithm". Follow the
     # provided pseudocode using NOT > AND > OR.
     # TODO(Task 1): complete the conversion.
-    raise NotImplementedError
+    for token in tokens:
+
+        # case of "("
+        if token == "(":
+            operator_stack.append(token)
+        # case of ")", we pop operator_stack into queue until ")" and then discard it
+        elif token == ")":
+            while operator_stack and operator_stack[-1] != "(":
+                output_queue.append(operator_stack.pop())
+            # pop "(" as we do not need it
+            operator_stack.pop()
+        
+        elif token == "NOT":
+            # if operator_stack is not empty or the token is "NOT" since "NOT" has highest 
+            operator_stack.append(token)
+
+        elif token in {"AND", "OR"}:
+            # AND has higher precedence over OR so if AND already exists on stack, then pop AND first into output_queue then push OR
+            while (operator_stack and operator_stack[-1] != "(" and precedence[operator_stack[-1]] >= precedence[token]):
+                output_queue.append(operator_stack.pop())
+            # otherwise the precedence order is respected e.g. OR then AND, we can simply push to stack
+            operator_stack.append(token)
+        # literal/term
+        else:
+            output_queue.append(token)
+
+    # final flush of remaining operators into output_queue
+    while operator_stack:
+        output_queue.append(operator_stack.pop())
+
+    return output_queue
 
 
 def _evaluate_postfix(postfix_tokens: List[str], index_path: str) -> Set[int]:
@@ -37,7 +72,9 @@ def _evaluate_postfix(postfix_tokens: List[str], index_path: str) -> Set[int]:
                 stack.append(left | right)
         elif op == "NOT":
             # TODO(Task 1): implement unary NOT.
-            raise NotImplementedError
+            # NOT peepoopeepee is defined as ALL DOCs - peepoopeepee
+            operand = stack.pop()
+            stack.append(set(get_all_doc_ids(index_path)) - operand)
         else:
             stack.append(set(get_posting_list(token, index_path)))
 
